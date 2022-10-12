6ba003dfcb338d81cabd953ae5a759beb8415947
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2022-10-11 22:32:35 -0300

    Refactored to deploy

--- next.config.js
+++ next.config.js
@@ -5,14 +5,16 @@
  */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+}),
+
+
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  },
+
   images: {
     domains: ['media.graphassets.com']
   },
