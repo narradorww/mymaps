ec908ddbfeb7ab26821fb5715eaf75b65b480a0d
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2022-10-12 00:09:47 -0300

    Refactored to deploy

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-/**
- * @type {import('next').NextConfig}
- */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+})
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  },
+const nextConfig = withPWA({
+  reactStrictMode: true,
   images: {
     domains: ['media.graphassets.com']
-  },
-  future: { webpack5: true },
-  reactStrictMode: true
+  }
 })
+
+module.exports = nextConfig
